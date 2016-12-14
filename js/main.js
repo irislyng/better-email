7900d602685f6f4bc5a1690efe876a9d1a4edd99
Mario Machado <mario.machado94@gmail.com>
2016-12-14 04:07:58 -0500

    added loading logic for email and email list

--- js/main.js
+++ js/main.js
@@ -1,8 +1,56 @@
-function loadEmail() {
+function loadEmailList() {
+	let parent = document.getElementById("email-list");
 	let emails = JSON.parse(localStorage.getItem("email_data"));
 	for (var i = 0; i < emails.length; i++) {
-		
+		let email = document.createElement("div");
+		let date = document.createElement("span");
+		let name = document.createElement("span");
+		let subject = document.createElement("span");
+		let content = document.createElement("span");
+
+		date.innerHTML = emails[i].datetime;
+		name.innerHTML = emails[i].first_name_from + " " + emails[i].last_name_from;
+		subject.innerHTML = emails[i].subject;
+		content.innerHTML = emails[i].content;
+
+		email.className = "message-preview";
+		email.setAttribute("onclick", "loadEmail(" + emails[i].id + ")");
+		date.className = "message-preview-date";
+		name.className = "message-preview-name";
+		subject.className = "message-preview-subject";
+		content.className = "message-preview-content";
+
+		email.appendChild(date);
+		email.appendChild(name);
+		email.appendChild(subject);
+		email.appendChild(content);
+
+		parent.appendChild(email);
 	}
+}
+
+function loadEmail(id) {
+	let emails = JSON.parse(localStorage.getItem("email_data"));
+	let email;
+	for (var i = 0; i < emails.length; i++) {
+		if (emails[i].id == id) {
+			email = emails[i];
+			break;
+		}
+	}
+
+	let subject = document.querySelector("#content-panel .content-message-subject");
+	let name = document.querySelector("#content-panel .content-message-sender");
+	let date = document.querySelector("#content-panel .content-message-date");
+	let temail = document.querySelector("#content-panel .content-message-email");
+	let content = document.querySelector("#content-panel .content-message-content");
+
+	subject.innerHTML = email.subject;
+	name.innerHTML = email.first_name_from + " " + email.last_name_from + " " 
+	+ "<span class='content-message-sender-email'>" + email.from + "</span>";
+	date.innerHTML = email.datetime;
+	temail.innerHTML = email.to;
+	content.innerHTML = email.content;
 }
 
 function composeEmail() {
@@ -26,3 +74,6 @@ function composeEmail() {
 	emails.push(email);
 	localstorage.setItem("email_data", JSON.stringify(emails));
 }
+
+loadEmailList();
+loadEmail(JSON.parse(localStorage.getItem("email_data"))[0].id)
