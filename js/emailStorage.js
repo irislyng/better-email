a37d59a7e31fa5d4561ae844441c3d12a5b57a36
Iris Ng <irislyng@gmail.com>
2016-12-14 22:10:01 -0500

    Fixed datetime formatting.

--- js/emailStorage.js
+++ js/emailStorage.js
@@ -31,21 +31,26 @@ function store(isSent){
 	var month = DateTime.getMonth() + 1;
 	var day = DateTime.getDate();
 	var hour = DateTime.getHours();
+	var minutes = DateTime.getMinutes();
 	var dd = "AM";
 
 	if (hour >= 12) {
         hour = hour - 12;
-        hour = "0" + hour;
+        if (hour < 10) hour = "0" + hour;
         dd = "PM";
     } else if (hour == 0) {
         hour = 12;
+    }
+
+    if (minutes < 10) {
+    	minutes = "0" + minutes;
     }
 
 	email.datetime =  year + "-" +
 				month + "-" +
 				day + " " +
 				hour + ":" +
-				DateTime.getMinutes() + " " + dd;
+				minutes + " "+ dd;
 
 	if(isSent){
 		email.folder.push("Sent");
@@ -53,7 +58,7 @@ function store(isSent){
 		email.folder.push("Drafts");
 	}
 
-	current.unshift(email);
+	current.push(email);
 	console.log(email);
 
 	localStorage.setItem("email_data", JSON.stringify(current));
